0e466c1eb49e4b434f69dc25651b544d8bc97a59
AdriR1ze <adriano.mancuso.2008@gmail.com>
2023-07-02 16:54:25 -0300

    Posibles movimientos de Torre y Peon

--- proyecto/Pieza.py
+++ proyecto/Pieza.py
@@ -1,37 +1,44 @@
 import TableroConceptual
 from typing import TypeVar, List
 from enum import Enum
+
+
 class TipoPieza(Enum):
-    ALFIL=1
-    TORRE=2
-    CABALLO=3
-    PEON=4
-    REY=5
-    REINA=6
+    ALFIL = 1
+    TORRE = 2
+    CABALLO = 3
+    PEON = 4
+    REY = 5
+    REINA = 6
+
+
 class TipoBando(Enum):
-    BLANCO=1
-    NEGRO=2
+    BLANCO = 1
+    NEGRO = 2
+
 
 class Pieza:
 
-    def __init__(self,tipo: TipoPieza, vivo: bool, posicion:  tuple[int, int], bando: TipoBando):
-        self.tipo=tipo
+    def __init__(self, tipo: TipoPieza, vivo: bool, posicion: tuple[int, int], bando: TipoBando):
+        self.tipo = tipo
         self.vivo = vivo
         self.posicion = posicion
         self.bando = bando
+
     def posibles_movimientos_de_alfil(self) -> List[tuple[int, int]]:
         lista_de_movimientos = []
         for columna in range(8):
-            columna=columna+1
+            columna = columna + 1
             for fila in range(8):
-                fila=fila+1
-                if abs((columna-self.posicion[0]))==abs((fila-self.posicion[1])) and (columna,fila)!=self.posicion:
-                    lista_de_movimientos.append((columna,fila))
+                fila = fila + 1
+                if abs((columna - self.posicion[0])) == abs((fila - self.posicion[1])) and (
+                columna, fila) != self.posicion:
+                    lista_de_movimientos.append((columna, fila))
         return lista_de_movimientos
 
     def posibles_movimientos_de_caballo(self) -> List[tuple[int, int]]:
-        lista_de_movimientos=[]
-        lista_de_movimientos.append((self.posicion[0]-2,self.posicion[1]-1))
+        lista_de_movimientos = []
+        lista_de_movimientos.append((self.posicion[0] - 2, self.posicion[1] - 1))
         lista_de_movimientos.append((self.posicion[0] - 2, self.posicion[1] + 1))
         lista_de_movimientos.append(((self.posicion[0] + 2), (self.posicion[1] - 1)))
         lista_de_movimientos.append(((self.posicion[0] + 2), (self.posicion[1] + 1)))
@@ -39,20 +46,51 @@ class Pieza:
         lista_de_movimientos.append(((self.posicion[0] - 1), (self.posicion[1] + 2)))
         lista_de_movimientos.append(((self.posicion[0] + 1), (self.posicion[1] - 2)))
         lista_de_movimientos.append(((self.posicion[0] - 1), (self.posicion[1] - 2)))
-        lista_de_movimientos=list(filter(lambda a:a[0]>0 and a[1]>0 and a[0]<=8 and a[1]<=8, lista_de_movimientos))
+        lista_de_movimientos = list(
+            filter(lambda a: a[0] > 0 and a[1] > 0 and a[0] <= 8 and a[1] <= 8, lista_de_movimientos))
+        return lista_de_movimientos
+
+    def posibles_movimientos_de_torre(self):
+        lista_de_movimientos = []
+        filas_para_arriba = self.posicion[1]
+        filas_para_abajo = self.posicion[1]
+        columnas_para_adelante = self.posicion[0]
+        columnas_para_atras = self.posicion[0]
+        for a in range(15):
+            columnas_para_adelante = columnas_para_adelante + 1
+            columnas_para_atras = columnas_para_atras - 1
+            filas_para_arriba = filas_para_arriba + 1
+            filas_para_abajo = filas_para_abajo - 1
+            if columnas_para_adelante<9:
+                lista_de_movimientos.append((columnas_para_adelante, self.posicion[1]))
+            if columnas_para_atras>0:
+                lista_de_movimientos.append((columnas_para_atras, self.posicion[1]))
+            if filas_para_abajo>0:
+                lista_de_movimientos.append((self.posicion[0], filas_para_abajo))
+            if filas_para_arriba<9:
+                lista_de_movimientos.append((self.posicion[0], filas_para_arriba))
+        return lista_de_movimientos
+    def posibles_movimientos_de_peon(self):
+        lista_de_movimientos=[]
+        lista_de_movimientos.append((self.posicion[1]+1,self.posicion[0]))
         return lista_de_movimientos
     def posibles_movimientos(self):
-        if TipoPieza.ALFIL==self.tipo:
+        if TipoPieza.ALFIL == self.tipo:
             return self.posibles_movimientos_de_alfil()
-        if TipoPieza.CABALLO==self.tipo:
+        if TipoPieza.CABALLO == self.tipo:
             return self.posibles_movimientos_de_caballo()
+        if TipoPieza.TORRE==self.tipo:
+            return self.posibles_movimientos_de_torre()
+        if TipoPieza.PEON==self.tipo:
+            return self.posibles_movimientos_de_peon()
 
-Alfil=Pieza(TipoPieza.ALFIL,True,(2,3),TipoBando.BLANCO)
 
-Caballo=Pieza(TipoPieza.CABALLO,True,(8,8),TipoBando.BLANCO)
+Alfil = Pieza(TipoPieza.ALFIL, True, (2, 3), TipoBando.BLANCO)
+Torre = Pieza(TipoPieza.TORRE, True, (4, 5), TipoBando.BLANCO)
+Caballo = Pieza(TipoPieza.CABALLO, True, (8, 8), TipoBando.BLANCO)
+Peon = Pieza(TipoPieza.PEON, True, (1, 1), TipoBando.BLANCO)
+
 print(Alfil.posibles_movimientos())
 print(Caballo.posibles_movimientos())
-
-
-
-
+print(Torre.posibles_movimientos())
+print(Peon.posibles_movimientos())
